f653d0672ff11d176d243d639813ac79514a7c7b
CSIRamone <cidinei_inacio@hotmail.com>
2020-02-22 17:45:13 -0300

    Select All from Department

--- src/db/DB.java
+++ src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -34,8 +36,33 @@ public class DB {
 			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
+		}		
+			
+    }
+	
+	public static void closeStatement(Statement st) {
+		if (st != null ) {
+			try {
+				st.close();	
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+			
 		}
-	}	
+			
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null ) {
+			try {
+				rs.close();	
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+			
+		}
+			
+	}
 	
 	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")){
