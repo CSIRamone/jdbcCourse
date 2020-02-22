c8e4e810e8a01e2d1915eb4c5ddefcaff08e746f
CSIRamone <cidinei_inacio@hotmail.com>
2020-02-22 18:39:00 -0300

    Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import com.mysql.jdbc.Statement;
 
 import db.DB;
 
@@ -15,56 +10,33 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	    Connection conn = null;
-	    PreparedStatement pt = null;
-	    
-	    try {
-	    	conn = DB.getConnection();
-	    	
-	    	pt = conn.prepareStatement(
-	    			"Insert into seller"
-	    			+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-	    			+ "values"
-	    			+ "(?, ?, ?, ?, ?)",
-	    			Statement.RETURN_GENERATED_KEYS);
-	    	
-	    	pt.setString(1, "Carl Purple");
-	    	pt.setString(2, "carl@gmail.com");
-            pt.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            pt.setDouble(4, 3000.0);
-            pt.setInt(5, 4);
-            
-            int rowsAffected = pt.executeUpdate();
-            
-            if (rowsAffected > 0) {
-            	ResultSet rs  = pt.getGeneratedKeys();
-            	while (rs.next()) {
-            		int id = rs.getInt(1);
-            		System.out.println("Done! Id = " + id);
-            	}
-            }
-            else {
-            	System.out.println("No rows Affected!");
-            }
-	    	
-	    			
-	    }
-	    catch (SQLException e) {
-	    	e.printStackTrace();
-	    }
-	    catch (ParseException e ) {
-	    	e.printStackTrace();
-	    }
-	    finally {
-	    	DB.closeStatement(pt);
-	    	DB.closeConnection();
-	    	
-	    }
-	    
-	   
-	    
-	  
+        
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+		    st = conn.prepareStatement(
+		    		"update seller "
+		    		+ "set BaseSalary = BaseSalary + ? "
+		    				+ "where "
+		    		+ "(DepartmentId = ?)");
+		    st.setDouble(1, 200.0);
+		    st.setInt(2,  2);
+		    
+		    int rowsAffected = st.executeUpdate();
+		    System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+				
+				
 	}
 
 }
